77622aaa7051df5af46ba28e8e9330cf49ce2a39
Bruno Sérvio Barbosa <brunoservio21@gmail.com>
2023-07-18 19:25:44 -0300

    feat: finishing touches on GET method

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,12 @@ export const routes = [
 		method: 'GET',
 		path: buildRoutePath('/users'),
 		handler: (req, res) => {
-			const users = database.select("users")
+			const { search } = req.query
+			const users = database.select("users", search ? {
+				name: search,
+				email: search
+			} : null)
+
 
 			return res.end(JSON.stringify(users));
 		}
@@ -32,6 +37,23 @@ export const routes = [
 		}
 	},
 	{
+		method: 'PUT',
+		path: buildRoutePath('/users/:id'),
+		handler: (req, res) => {
+			const { id } = req.params
+			const { name, email } = req.body
+
+			const data = {
+				name,
+				email,
+			}
+
+			database.update("users", id, data)
+
+			return res.writeHead(204).end()
+		}
+	},
+	{
 		method: 'DELETE',
 		path: buildRoutePath('/users/:id'),
 		handler: (req, res) => {
@@ -41,5 +63,5 @@ export const routes = [
 
 			return res.writeHead(204).end()
 		}
-	}
+	},
 ]
