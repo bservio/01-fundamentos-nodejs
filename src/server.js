804a521777c356e3f182d2c6690484f21ce57c63
Bruno Sérvio Barbosa <brunoservio21@gmail.com>
2023-07-18 18:11:39 -0300

    feat: regex to capture id

--- src/server.js
+++ src/server.js
@@ -9,10 +9,12 @@ const server = http.createServer(async (req, res) => {
 	await json(req, res)
 
 	const route = routes.find(r => {
-		return r.path === url && r.method === method
+		return r.method === method && r.path.test(url)
 	})
 
 	if (route) {
+		const routeParams = req.url.match(route.path)
+		console.log(routeParams)
 		return route.handler(req, res)
 	}
 
